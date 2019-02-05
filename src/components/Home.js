93079f33c895bc90d45cee4233321e58de15ee25
Mossy <bridgo.mcmahon@gmail.com>
2019-02-05 18:24:17 +1100

    added Home component and Routes

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+ import React, { Component } from 'react';
+ import Link from 'react-router-dom';
+
+ class Home extends Component {
+   render() {
+     return (
+       <div>
+        <h1>Burning Airlines</h1>
+       </div>
+     );
+   }
+ }
