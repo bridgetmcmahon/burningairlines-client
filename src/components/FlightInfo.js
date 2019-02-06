37c623bca714911c5dfc8a3d1b95d80ead04d81f
Shouyi Wang <198784@gmail.com>
2019-02-07 01:15:59 +1100

    Flight info page update

--- src/components/FlightInfo.js
+++ src/components/FlightInfo.js
@@ -1,57 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import '../App.css'
 
 
 class FlightInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flight: [],
+      flight: {},
       planeID: 0,
       airplane: {},
-      totalRows: [],
-      totalCols: [],
+      rows: 0,
+      columns: 0,
+      reservations: {}
     }
   }
 
   componentDidMount() {
     const id = this.props.match.params.id
 
-    let FLIGHT_URL = `http://localhost:4000/flights/${ id }.json`
-
+    const FLIGHT_URL = `http://localhost:3000/flights/${ id }.json`
     axios.get(FLIGHT_URL).then( (results) => {
       this.setState({
         flight: results.data,
         planeID: results.data.airplane_id
       });
 
-      let AIRPLANE_URL = `http://localhost:4000/airplanes/${ this.state.planeID }.json`
 
+      const AIRPLANE_URL = `http://localhost:3000/airplanes/${ this.state.planeID }.json`
       axios.get(AIRPLANE_URL).then( (results) => {
 
         let plane = results.data;
-        let rowArray = new Array(plane.rows).fill(null);
-        let colArray = new Array(plane.columns).fill(null);
-
-        for (var i = 0; i < rowArray.length; i++) {
-          rowArray[i] = i + 1;
-        }
-
-        for (var i = 0; i < colArray.length; i++) {
-          colArray[i] = i + 1;
-        }
-
-        console.log(rowArray, colArray);
-
         this.setState({
           airplane: results.data,
-          totalRows: rowArray,
-          totalCols: colArray,
+          rows: plane.rows,
+          columns: plane.columns
         });
-        // console.log(this.state);
+
+        const RESERVATION_URL = `http://localhost:3000/reservations.json`
+        axios.get(RESERVATION_URL).then( (results) => {
+
+          let reservations = results.data.filter(r => r.flight_id == id);
+          this.setState({
+            reservations: reservations
+          });
+        });
       });
     });
+
   }
 
   render() {
@@ -71,11 +68,9 @@ class FlightInfo extends Component {
             Destination: { this.state.flight.destination }
           </p>
 
-          <div>
-            { this.state.totalRows.map( (rowNo) =>
-              <Row />
-            )}
-          </div>
+
+          <Reservation data={ this.state }/>
+
 
         <Link to="/">Back to Home</Link>
       </div>
@@ -86,12 +81,41 @@ class FlightInfo extends Component {
   }
 }
 
-class Row extends Component {
+class Reservation extends Component {
+
+  createTable = () => {
+    let d = this.props.data;
+    let reservedSeats = [];
+    let table = [];
+
+    Array.from(d.reservations).forEach(function(r) {reservedSeats.push(r.seat);});
+
+   for (let i = 1; i <= d.rows; i++) {
+     let children = [];
+     for (let j = 1; j <= d.columns; j++) {
+       let seat = i +  String.fromCharCode(64 + j);
+       if(reservedSeats.indexOf(seat) > -1) {
+         children.push(<td>{`${seat}`} </td>);
+       }
+       else {
+         children.push(<td> |___| &nbsp; &nbsp;</td>);
+       }
+
+     }
+
+     table.push(<tr>{children}</tr>);
+   }
+   return table;
+ }
+
+
   render() {
+
+
     return (
-      <div>
-        Seat
-      </div>
+      <table>
+        {this.createTable()}
+      </table>
     );
   }
 }
