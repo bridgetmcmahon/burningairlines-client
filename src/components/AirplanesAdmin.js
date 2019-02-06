6cbc71f37f484fafb1a6fc4bf39d93337822eea5
Elena Ovsyannikova <helenovsau@gmail.com>
2019-02-06 16:24:48 +1100

    some changes AirplanesAdmin

--- src/components/AirplanesAdmin.js
+++ src/components/AirplanesAdmin.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Home from './Home';
 import axios from 'axios';
 
-const SERVER_URL = "http://localhost:3000/airplanes.json"
+const SERVER_URL = "http://localhost:4000/airplanes.json"
 
 class AirplanesAdmin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plane_model: '',
+      rows: '',
+      columns: '',
+    }
+    this._handleInputPlaneModel = this._handleInputPlaneModel.bind(this);
+  }
+  _handleInputPlaneModel(e) {
+    // console.log(e.target.value)
+    this.setState({ plane_model: e.target.value });
+  }
   render() {
     return (
       <div className="container">
@@ -12,9 +26,9 @@ class AirplanesAdmin extends Component {
         <h2>Airplanes - Admin</h2>
 
         <form>
-          <input type="text" placeholder="name" />
-          <input type="text" placeholder="rows" />
-          <input type="text" placeholder="columns" />
+          <input type="text" placeholder="plane model" required onInput={ this._handleInputPlaneModel } />
+          <input type="text" placeholder="rows" required onInput={ this._handleInputPlaneRows } />
+          <input type="text" placeholder="columns" required onInput={ this._handleInputPlaneColumns } />
           <input type="submit" value="Add Airplane" />
         </form>
 
